47839d04a455778c094d10b6e7a1fe4ea2fcce23
tomato216 <phamthithanhhiencp2006@gmail.com>
2026-06-25 23:58:28 +0700

    Refactor fileScript entries in data.js

--- data.js
+++ data.js
@@ -5,13 +5,20 @@ const DATA_QUIZ = [
         bodeTitle: "Bộ đề số 1",
         chuongTitle: "Chương 2: Auguste Comte",
         difficulty: "MỨC ĐỘ: DỄ",
-        fileScript: "de-bai-1.js" // Liên kết tới file đề 1
+        fileScript: "de-bai-1.js"
     },
     {
         id: "bode-2",
         bodeTitle: "Bộ đề số 2",
         chuongTitle: "Chương 3: Karl Marx",
         difficulty: "MỨC ĐỘ: DỄ",
-        fileScript: "de-bai-2.js" // Liên kết tới file đề 2 mới
+        fileScript: "de-bai-2.js"
+    },
+    {
+        id: "bode-3",
+        bodeTitle: "Bộ đề số 3",
+        chuongTitle: "Chương 4: Herbert Spencer",
+        difficulty: "MỨC ĐỘ: DỄ",
+        fileScript: "de-bai-3.js" // Liên kết tới file đề 3 mới
     }
 ];
